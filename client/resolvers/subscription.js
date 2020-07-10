abeee1cd9eda890d1b6abd1cd5dfea9eac52edf9
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-07-10 13:13:57 +0530

    Update subscription.js

--- client/resolvers/subscription.js
+++ client/resolvers/subscription.js
@@ -1,11 +1,8 @@
 const Subscription = {
-
     notification:{
       subscribe(parent, args, {pubsub}){
         return pubsub.asyncIterator('notification');
        }
     }
-
-
 }
 export default Subscription;
