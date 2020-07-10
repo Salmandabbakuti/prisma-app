cc5bec4dd584e782da72d45df2e903039f7d900b
Salman Dabbakuti <dabbakutisalman@gmail.com>
2020-07-10 13:12:33 +0530

    Update mutation.js

--- client/resolvers/mutation.js
+++ client/resolvers/mutation.js
@@ -8,6 +8,64 @@ const Mutation = {
 
        console.log(`created new movie: ${newMovie}`);
        return newMovie;
+       pubsub.publish('notification', {
+        notification:{
+            event: 'New Movie Listed.',
+            data: newMovie
+        }
+      })
+       },
+
+
+    async updateMovie(parent, args, { prisma }) {
+       
+       let isMovieExists = await prisma.exists.Movie({id:args.id})
+        if(!isMovieExists){
+            throw new Error(`Movie doesnot exist with this Id`)
+        }
+       return await prisma.mutation.updateMovie({
+           
+           where:{
+               id:args.id
+               },
+            data:{
+                name:args.name,
+                rating:args.rating,
+                rank:args.rank,
+                producer:args.producer
+            } 
+         })
+        pubsub.publish('notification', {
+        notification:{
+            event: 'Movie Data Updated',
+            data: {
+             name: args.name,
+             producer: args.producer,
+             rating: args.rating,
+             rank: args.rank
+             }
+           }
+         })
+       },
+
+    async deleteMovie(parent, args, { prisma }) {
+       
+       let isMovieExists = await prisma.exists.Movie({id:args.id})
+        if(!isMovieExists){
+            throw new Error(`Movie doesnot exist with this Id`)
+        }
+       await prisma.mutation.deleteMovie({
+           where:{
+               id:args.id
+               }
+         })
+        return ({response:"Movie Deletd Successfully"})
+         pubsub.publish('notification', {
+          notification:{
+              event: 'Movie Deleted',
+              data: {name:args.id}
+                 }
+              })
        }
    }
 export default Mutation;
